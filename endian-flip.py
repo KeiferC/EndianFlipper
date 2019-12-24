e1c34520671800fb62431930c75fb0f231b012d5
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2019-12-24 12:57:47 -0800

    fixed docs

--- endian-flip.py
+++ endian-flip.py
@@ -23,18 +23,14 @@ import sys
 # Main                                  #
 #########################################
 # 
-# main
-#
 # Validates input, parses command-line arguments, runs program.
 #
 # @param        n/a
 # @return       n/a
 #
 def main():
-        # TODO
-
         if (valid_input()):
-                flip(parse())
+                print(*flip_endian(parse_infile()))
                 sys.exit()
         else:
                 usage()
@@ -45,12 +41,10 @@ def main():
 # Functions                             #
 #########################################
 # 
-# flip_endian
-#
 # Given a dictionary containing a number of bits for an integer
-# representation and an array of hex values, flips endianness of  the hex 
+# representation and an array of hex values, flips endianness of hex 
 # values in accordance with the integer representation. Returns an array 
-# of flipped hex values
+# of flipped hex values.
 #
 # @param        {dictionary} flipper_args: contains the following kv-pairs
 #                       {number} int_size: # bits for int representation
@@ -60,22 +54,35 @@ def main():
 def flip_endian(flipper_args):
         # TODO
 
-        return None
+        return ["test", "one"]
 
 
+#########################################
+# Command-Line Parsing                  #
+#########################################
 #
-# TODO
+# Returns true if the command-line arguments are valid. Currently not 
+# robust at all for speedy scripting and use.
+#
+# @param        n/a
+# @returns      {boolean} True if arguments are valid
 #
 def valid_input():
         # TODO
 
         return True
 
-
 #
-# TODO
+# Retrieves command-line arguments from system and parses given file
+# accordingly. Returns a dictionary containing integer representation
+# and array of hex values from file.
 #
-def parse():
+# @param        n/a
+# @returns      {dictionary} flipper_args: contains the following kv-pairs
+#                       {number} int_size: # bits for int representation
+#                       {list} hex_arr: array of hex values to flip
+#
+def parse_infile():
         # TODO
 
         flipper_args = {"num_bits": None, "hex_arr": None}
